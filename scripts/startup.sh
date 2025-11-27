c488239a6c00263eaac1f1b993d9439dc86c6362
Sash1ro <matteoplancher@icloud.com>
2025-11-27 21:59:46 +0100

    updated installation

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,10 +2,6 @@
 
 source $HOME/.config/hyprlab/scripts/data/conf.env
 
-scripts="$HOME/.config/hyprlab/scripts"
-
 nvim --headless -c "TransparentEnable" -c "qa!" && echo "error"
-
-swww-daemon & waybar & swaync & hyprsunset
 
 swww img "$THEMES_DIR/current/wallpaper" --transition-type grow --transition-fps 60
