822253fe6db5fa6e337091fa77e3a6394f056ef6
Sash1ro <matteoplancher@icloud.com>
2025-12-20 14:00:07 +0100

    new wallpaper picker

--- scripts/menu/wallpaper-switcher.sh
+++ scripts/menu/wallpaper-switcher.sh
@@ -4,22 +4,14 @@ source $HOME/.config/hyprlab/scripts/data/conf.env
 
 current="$THEMES_DIR/current"
 currentWallpaper="$current/wallpaper"
-CACHE_ROOT="$SCRIPT_DIR/cache/cached_imgs"
-WALLPAPER_ROOT="$current/wallpapers"
-l=1
 
 case $1 in
-    "all") WALLPAPER_ROOT="$HYPRLAB/wallpapers" 
-           l=3;;
-    *);;
+    all) WALLPAPER_ROOT="$HYPRLAB/wallpapers";;
+    *)WALLPAPER_ROOT="$current/wallpapers";;
 esac
 
-[ ! -d $CACHE_ROOT ] && mkdir -p $CACHE_ROOT
-
 mapfile -t originPath < <(find ${WALLPAPER_ROOT} -maxdepth 1 -type f)
-mapfile -t cachedPath < <(find ${CACHE_ROOT} -maxdepth 1 -type f)
 declare -A bgresult
-declare -A cachedresult
 
 bgnames=()
 
@@ -28,19 +20,6 @@ if pgrep -x "rofi" >/dev/null; then
     pkill rofi
     exit 0
 fi
-
-function cacheImg {
-    resolution=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 $1)
-    width_=$(echo "${resolution}" | awk -F',' '{print $1}')
-    height_=$(echo "${resolution}" | awk -F',' '{print $2}')
-    if [ "${width_}" -lt "${height_}" ]
-    then
-        ffmpeg -i $1 -loglevel quiet -vf  "scale=600:-1, crop=600:600:0:(iw-600)/2" $2
-    else
-        ffmpeg -i $1 -loglevel quiet -vf  "scale=-1:600, crop=600:600:(iw-600)/2:0" $2
-    fi
-    echo $2
-}
 
 function getFileName {
     echo "$1" | xargs basename | awk -F'.' '{print $1}'
@@ -53,32 +32,13 @@ for pathIDX in "${!originPath[@]}"; do
 done
 
 
-for pathIDX in "${!cachedPath[@]}"; do
-    filename=$(getFileName "${cachedPath[$pathIDX]}")
-    cachedresult["${filename}"]="${cachedPath[$pathIDX]}"
-done
-
-test=0
-for fName in "${bgnames[@]}"; do
-    if [[ -v cachedresult[$fName] ]] 
-    then
-        :
-    else
-        if [[ $test -eq 0 ]]; then
-            hyprlab notify normal "Hyprlab" "Caching images" "This operation could take some times.."
-            test=1
-        fi
-        cachedresult[$fName]=$(cacheImg "${bgresult[$fName]}" "${CACHE_ROOT}/${fName}.png")
-    fi
-done
-
 strrr=""
 for fName in "${bgnames[@]}"; do
-    strrr+="$(echo -n "${fName}\0icon\x1f${cachedresult[$fName]}\n")"
+    strrr+="$(echo -n "${fName}\0icon\x1f${bgresult[$fName]}\n")"
 done
 
 
-selected=$(echo -en "${strrr}" | sort | rofi -dmenu -disable-history -i -l $l -theme $ROFI_THEME/wallpapersPicker.rasi)
+selected=$(echo -en "${strrr}" | sort | PREVIEW=true rofi -dmenu -disable-history -i -theme $ROFI_THEME/wallpapersPicker.rasi)
 
 if [ -z "$selected" ]; then
     exit 0
